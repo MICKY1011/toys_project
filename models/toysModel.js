2c800fb66782efe839f147ef490a0804c75c306f
michael <108010812+MICKY1011@users.noreply.github.com>
2023-09-19 10:39:23 +0300

    Add files via upload

---
+++ models/toysModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const ToysSchema = new mongoose.Schema(
+  {
+    name: String,
+    info: String,
+    category: String,
+    img_url: String,
+    price: Number,
+    user_id: String,
+  },
+  { timestamps: true }
+);
+exports.ToysModel = mongoose.model("toys", ToysSchema);
+
+exports.validateToys = (reqBody) => {
+  const JoiSchema = Joi.object({
+    name: Joi.string().min(2).max(99).required(),
+    info: Joi.string().min(2).max(999).required(),
+    category: Joi.string().min(2).max(999).required(),
+    img_url: Joi.string().min(2).max(999).allow(null ,""),
+    price: Joi.number().min(1).max(99999).required(),
+  });
+  return JoiSchema.validate(reqBody);
+};
